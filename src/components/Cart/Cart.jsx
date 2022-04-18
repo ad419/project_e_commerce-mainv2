1f71b2a7c4ed9474919387d3692245d1081288af
ad419 <64656327+ad419@users.noreply.github.com>
2022-04-18 22:31:48 +0200

    oke

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Typography, Button, Grid } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
